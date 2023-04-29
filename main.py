df448e6a9aae21b40fa6c4e25a707d6a80943946
Rick Gladwin <rick@iddesign.ca>
2023-04-29 00:31:16 -0400

    add main.py and board print functions

---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+
+null_board = [None for i in range(0, 10)]
+example_board = [None, None, 'X', None, 'O', 'X', None, 'O', None]
+
+
+def print_board(board):
+    vert = '|'
+    horiz = '–'
+    cross = '+'
+
+    uprights = [1, 3, 11, 13, 21, 23]
+    horizontals = [5, 7, 9, 15, 17, 19]
+    crosses = [6, 8, 16, 18]
+
+    # symbols = uprights + horizontals + crosses
+
+    lasts = [4, 9, 14, 19]
+
+    i = 0
+    # print top row
+    while i < 25:
+        is_symbol = False
+        if i in uprights:
+            sys.stdout.write(vert)
+            is_symbol = True
+        if i in horizontals:
+            sys.stdout.write(horiz)
+            is_symbol = True
+        if i in crosses:
+            sys.stdout.write(cross)
+            is_symbol = True
+        if not is_symbol:
+            board_entry = board.pop(0)
+            if board_entry is None:
+                board_entry = '#'
+            sys.stdout.write(board_entry)
+        if i in lasts:
+            print('')
+        i = i + 1
+
+
+def print_board_simple(board):
+    lasts = [2, 5, 8]
+
+    i = 0
+
+    while i < 9:
+        board_entry = board.pop(0)
+        if board_entry is None:
+            board_entry = '#'
+        sys.stdout.write(board_entry)
+        if i in lasts:
+            print('')
+        i = i + 1
+
+
+def main():
+    print('starting...')
+
+
+if __name__ == "__main__":
+    print('example board:')
+    print_board_simple(example_board)

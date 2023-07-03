cae1536e74295a859ab5dfd01ec442be96155a46
Rick Gladwin <rick@iddesign.ca>
2023-07-03 12:41:05 -0400

    add linear best fit line to learning proxy visualization plot (last 2000 points)

--- visualizations.py
+++ visualizations.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from database import get_blank_weights, iterable_from_weights, get_blank_weights_from_history
 from settings import settings
@@ -18,6 +19,17 @@ def draw_blank_weights_over_time(opponent_name, opponent_char) -> None:
     ax.set_xlabel('game number')
     ax.set_ylabel('blank state weights sum')
     ax.plot(x, y)
+
+    # draw line of best fit for last N points
+    latest_range = 2000
+    x_best = np.array(list(x)[-latest_range:])
+    y_best = np.array(y[-latest_range:])
+    a, b = np.polyfit(x=x_best, y=y_best, deg=1)
+    # print(f'last x_best: {x_best[-10:]}')
+    # print(f'last y_best: {y_best[-10:]}')
+    # print(f'a,b: {a}, {b}')
+    plt.plot(x, a*x + b)
+
     plt.show()
 
 
